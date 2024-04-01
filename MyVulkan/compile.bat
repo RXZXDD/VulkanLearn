ad14dd2b7bddd81a7084a7e8a092abf6095b5e64
RXZXDD <424376816@qq.com>
2024-04-01 09:28:28 +0800

    Revert "本地化complie path"
    This reverts commit 5ef86635d60b34228e1ecc931b1b0606783a95b7.

--- MyVulkan/compile.bat
+++ MyVulkan/compile.bat
@@ -1,3 +1,3 @@
-C:\VulkanSDK\1.3.268.0\Bin\glslc.exe shaders\simple_shader.vert -o shaders\simple_shader.vert.spv
-C:\VulkanSDK\1.3.268.0\Bin\glslc.exe shaders\simple_shader.frag -o shaders\simple_shader.frag.spv
+G:\VulkanSDK\1.3.280.0\Bin\glslc.exe shaders\simple_shader.vert -o shaders\simple_shader.vert.spv
+G:\VulkanSDK\1.3.280.0\Bin\glslc.exe shaders\simple_shader.frag -o shaders\simple_shader.frag.spv
 pause
